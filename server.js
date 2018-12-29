1e1e45bde6036b22c3059b67b68f897a355bc507
Shawn Stewart <s.stewart0220@gmail.com>
2018-12-28 22:44:16 -0500

    Server is connecting to mLab

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+require("dotenv").load();
 const express = require("express");
 const mongoose = require("mongoose");
 const app = express();
@@ -7,17 +8,17 @@ app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
 // MongoDB Config
-// const db =
+const db = process.env.MONGO_URI;
 mongoose
   .connect(
     db,
     { useNewUrlParser: true }
   )
   .then(() => {
-    console.log("=== Connected to MongoDB ===");
+    console.log("=== Connected to MongoDB ===\n");
   })
   .catch(err => {
     console.log("=== ERROR ===\n", err);
   });
 
-app.listen(port, () => console.log(`Server running on port ${port}`));
+app.listen(port, () => console.log(`\nServer running on port ${port}`));
