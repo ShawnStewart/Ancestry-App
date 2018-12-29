299864e21659394d8076989c1fbbe2b6e98f644e
Shawn Stewart <s.stewart0220@gmail.com>
2018-12-29 14:44:56 -0500

    Created indexes to the username and email field

--- models/User.js
+++ models/User.js
@@ -5,7 +5,8 @@ const UserSchema = new Schema({
   username: {
     type: String,
     required: true,
-    unique: true
+    unique: true,
+    index: true
   },
   password: {
     type: String,
@@ -13,7 +14,8 @@ const UserSchema = new Schema({
   },
   email: {
     type: String,
-    required: true
+    required: true,
+    index: true
   },
   createdAt: {
     type: Date,
