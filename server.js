a86234ff5a1668904fad476a9f2881ef822f4956
Shawn Stewart <s.stewart0220@gmail.com>
2018-12-29 13:56:37 -0500

    Connected user route to server

--- server.js
+++ server.js
@@ -7,6 +7,8 @@ const port = process.env.PORT || 5000;
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
+const users = require("./routes/api/user");
+
 // MongoDB Config
 const db = process.env.MONGO_URI;
 mongoose
@@ -20,5 +22,8 @@ mongoose
   .catch(err => {
     console.log("=== ERROR ===\n", err);
   });
+
+// Connect routes
+app.use("/api/users", users);
 
 app.listen(port, () => console.log(`\nServer running on port ${port}`));
